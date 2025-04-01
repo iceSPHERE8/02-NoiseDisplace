63b3ab27f01c6b5f10f04dda7fd0402addd47d57
fatoooooony <jacknasppear@gmail.com>
2025-04-01 17:16:25 +0800

    almost finish the main code &add leva GUI

--- src/components/three-components/poster-plane.jsx
+++ src/components/three-components/poster-plane.jsx
@@ -1,6 +1,8 @@
 import * as THREE from "three";
+import { useRef } from "react";
 import { useTexture, shaderMaterial } from "@react-three/drei";
-import { extend } from "@react-three/fiber";
+import { extend, useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
 
 import waveVertexShader from "./shaders/wave/vertex.glsl";
 import waveFragmentShader from "./shaders/wave/fragment.glsl";
@@ -9,6 +11,9 @@ const PosterPlaneShaderMate = shaderMaterial(
   {
     uTexture: null,
     uResolution: new THREE.Vector2(1080, 1920),
+    uTime: 0,
+    uScale: new THREE.Vector2(1, 1),
+    uStrength: 0
   },
   waveVertexShader,
   waveFragmentShader
@@ -17,9 +22,47 @@ const PosterPlaneShaderMate = shaderMaterial(
 extend({ PosterPlaneShaderMate });
 
 export default function PosterPlane() {
+  /**
+   * GUI Parameters
+   */
+  const { speed, noiseScaleX, noiseScaleY, displaceStrength } = useControls({
+    speed: {
+      value: 1,
+      min: 0,
+      max: 10,
+      step: 0.01
+    },
+    noiseScaleX: {
+      value: 1,
+      min: 0,
+      max: 50,
+      step: 0.001
+    },
+    noiseScaleY: {
+      value: 1,
+      min: 0,
+      max: 50,
+      step: 0.001
+    },
+    displaceStrength: {
+      value: 1,
+      min: 0, 
+      max: 1,
+      step: 0.001
+    }
+  })
+
   const posterImage = useTexture("../../poster.jpg");
   const posterImageWidth = posterImage.image.width;
   const posterImageHeight = posterImage.image.height;
+
+  const shader = useRef();
+
+  useFrame((state) => {
+    shader.current.uniforms.uTime.value = state.clock.elapsedTime * speed;
+    shader.current.uniforms.uScale.value = new THREE.Vector2(noiseScaleX, noiseScaleY);
+    shader.current.uniforms.uStrength.value = displaceStrength;
+  })
 
   return (
     <mesh>
@@ -32,6 +75,8 @@ export default function PosterPlane() {
       <posterPlaneShaderMate
         uTexture={posterImage}
         uResolution={[posterImageWidth, posterImageHeight]}
+        uScale={[noiseScaleX, noiseScaleY]}
+        ref={shader}
       />
     </mesh>
   );
--- src/components/three-components/shaders/wave/fragment.glsl
+++ src/components/three-components/shaders/wave/fragment.glsl
@@ -1,4 +1,7 @@
 uniform sampler2D uTexture;
+uniform float uTime;
+uniform vec2 uScale;
+uniform float uStrength;
 
 varying vec2 vUv;
 
@@ -6,8 +9,30 @@ varying vec2 vUv;
 #include "../voronoi2D.glsl"
 
 void main() {
-    vec3 color = texture2D(uTexture, vUv).rgb;
+    vec2 voroiPara = vec2((cnoise(vec2(vUv.x * uScale.x, vUv.y * uScale.y)) + uTime * 0.1), cnoise(vUv));
+    vec2 displace = cellular(voroiPara * 2.0);
 
-    // vec3 color = vec3(noise(vUv * 10.0));
+    displace.x = smoothstep(0.3, 1.0, displace.x);
+    displace.y = smoothstep(0.3, 1.0, displace.y);
+    vec2 newUv = vec2(vUv.x, vUv.y + displace.y * uStrength);
+
+    newUv = mix(vUv, newUv, displace.x);
+
+    // Create the edge gradient 
+    vec2 border = vUv;
+    border.x = clamp(pow(vUv.x, 10.0) + 0.2, 0.0, 1.0);
+    border.y = clamp(pow(vUv.y, 10.0) + 0.2, 0.0, 1.0);
+
+    float newBorderX = clamp(pow(1.0 - vUv.x, 10.0) + 0.2, 0.0, 1.0);
+    float newBorderY = clamp(pow(1.0 - vUv.y, 10.0) + 0.2, 0.0, 1.0);
+
+    float edge = border.x + border.y + newBorderX + newBorderY;
+    edge = smoothstep(0.8, 2.0, edge);
+    
+    newUv = mix(newUv, vUv, edge);
+
+    vec3 color = texture2D(uTexture, newUv).rgb;
+    // vec3 color = vec3(edge);
+
     gl_FragColor = vec4(color, 1.0);
 }
